5edb2791a5a1fb4cd551d41f2170234a0aff0244
skad0 <mrskadi0@gmail.com>
2022-08-14 22:10:18 +0300

    initial component added

---
+++ src/IntlTelInput.story.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { Variant } from '@vitebook/client';
+  import IntlTelInput from './IntlTelInput.svelte';
+</script>
+
+<Variant name="default">
+  <IntlTelInput />
+</Variant>
---
+++ src/IntlTelInput.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import intlTelInput from 'intl-tel-input/build/js/intlTelInput';
+  import type { Options } from 'intl-tel-input';
+
+  import 'intl-tel-input/build/css/intlTelInput.css';
+
+  export let initialOptions: Options = {};
+
+  let inputRef: HTMLElement;
+
+  onMount(async () => {
+    await import('intl-tel-input/build/js/utils');
+
+    intlTelInput(inputRef, { ...initialOptions });
+  });
+</script>
+
+<input on:click on:blur bind:this={inputRef} type="tel" {...$$restProps} />
